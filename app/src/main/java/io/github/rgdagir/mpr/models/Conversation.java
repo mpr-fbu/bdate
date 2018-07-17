a8221ef2b111d700f420c6e57c7e5d0c37582d3f
rlxu <xu.ruili99@gmail.com>
2018-07-17 16:47:33 -0700

    Create models for data

--- app/src/main/java/io/github/rgdagir/mpr/models/Conversation.java
+++ app/src/main/java/io/github/rgdagir/mpr/models/Conversation.java
@@ -1,4 +1,40 @@
 package io.github.rgdagir.mpr.models;
 
-public class Conversation {
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+@ParseClassName("Conversation")
+public class Conversation extends ParseObject {
+    private static final String KEY_USER1 = "user1";
+    private static final String KEY_USER2 = "user2";
+
+    public ParseUser getUser1() {
+        return getParseUser(KEY_USER1);
+    }
+
+    public void setUser1(ParseUser user) {
+        put(KEY_USER1, user);
+    }
+
+    public ParseUser getUser2() {
+        return getParseUser(KEY_USER2);
+    }
+
+    public void setUser2(ParseUser user) {
+        put(KEY_USER2, user);
+    }
+
+    public static class Query extends ParseQuery<Conversation> {
+        public Query() {
+            super(Conversation.class);
+        }
+
+        public Conversation.Query withUser() {
+            include(KEY_USER1);
+            include(KEY_USER2);
+            return this;
+        }
+    }
 }
--- app/src/main/java/io/github/rgdagir/mpr/models/Message.java
+++ app/src/main/java/io/github/rgdagir/mpr/models/Message.java
@@ -1,4 +1,49 @@
 package io.github.rgdagir.mpr.models;
 
-public class Message {
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+@ParseClassName("Message")
+public class Message extends ParseObject {
+    private static final String KEY_SENDER = "sender";
+    private static final String KEY_CONVO = "conversation";
+    private static final String KEY_TEXT = "text";
+
+    public ParseUser getSender() {
+        return getParseUser(KEY_SENDER);
+    }
+
+    public void setSender(ParseUser user) {
+        put(KEY_SENDER, user);
+    }
+
+    public Conversation getConversation() {
+        return (Conversation) getParseObject(KEY_CONVO);
+    }
+
+    public void setConversation(Conversation conversation) {
+        put(KEY_CONVO, conversation);
+    }
+
+    public String getText() {
+        return getString(KEY_TEXT);
+    }
+
+    public void setText(String text) {
+        put(KEY_TEXT, text);
+    }
+
+    public static class Query extends ParseQuery<Message> {
+        public Query() {
+            super(Message.class);
+        }
+
+        public Message.Query withConvoSender() {
+            include(KEY_CONVO);
+            include(KEY_SENDER);
+            return this;
+        }
+    }
 }
