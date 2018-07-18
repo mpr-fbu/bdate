d0fdfc30065c29e3e1a1ac6824e0c8f7d690dde5
Patricia Ouyang <pso91399@gmail.com>
2018-07-17 17:23:44 -0700

    Added ProfileFragment and SearchFragment and Actionbar functionality

--- app/src/main/java/io/github/rgdagir/mpr/ConversationAdapter.java
+++ app/src/main/java/io/github/rgdagir/mpr/ConversationAdapter.java
@@ -1,19 +1,5 @@
 package io.github.rgdagir.mpr;
 
-import android.app.Activity;
-import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.text.Spannable;
-import android.text.SpannableStringBuilder;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import com.parse.ParseImageView;
-
-import java.util.List;
-
 public class ConversationAdapter {
 
 }
---
+++ app/src/main/java/io/github/rgdagir/mpr/SearchFragment.java
@@ -0,0 +1,52 @@
+package io.github.rgdagir.mpr;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SearchFragment extends Fragment {
+    private SearchFragment.OnFragmentInteractionListener mListener;
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof SearchFragment.OnFragmentInteractionListener) {
+            mListener = (SearchFragment.OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // Placeholder, to be inserted when clicking is introduced
+        void onFragmentInteraction(Uri uri);
+    }
+
+}
