ce5b7a41284880a64a6f3958a82c7218823e2418
rlxu <xu.ruili99@gmail.com>
2018-07-18 18:10:23 -0700

    Add chat bubble layouts

--- app/src/main/java/io/github/rgdagir/mpr/ChatActivity.java
+++ app/src/main/java/io/github/rgdagir/mpr/ChatActivity.java
@@ -1,7 +1,11 @@
 package io.github.rgdagir.mpr;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.parse.ParseUser;
@@ -14,6 +18,8 @@ public class ChatActivity extends AppCompatActivity {
 
     Conversation conversation;
     private TextView tvUsername;
+    private Button btnReturn;
+    private RecyclerView rvMessages;
     ParseUser currUser;
 
     @Override
@@ -23,12 +29,22 @@ public class ChatActivity extends AppCompatActivity {
 
         conversation = Parcels.unwrap(getIntent().getParcelableExtra("conversation"));
         tvUsername = findViewById(R.id.tvUsername);
+        btnReturn = findViewById(R.id.btnReturn);
+
         currUser = ParseUser.getCurrentUser();
         if (currUser.getObjectId().equals(conversation.getUser1().getObjectId())) {
             tvUsername.setText(conversation.getUser2().getUsername());
         } else {
             tvUsername.setText(conversation.getUser1().getUsername());
         }
+
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChatActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // Then do querying and stuff and actually get the messages
 
