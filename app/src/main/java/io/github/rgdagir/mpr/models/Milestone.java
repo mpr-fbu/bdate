8a2088eb36d32c6df9ce6b993841b70f7d5c9f15
Patricia Ouyang <pso91399@gmail.com>
2018-07-20 09:34:09 -0700

    More Milestone model setup

--- app/src/main/java/io/github/rgdagir/mpr/models/Milestone.java
+++ app/src/main/java/io/github/rgdagir/mpr/models/Milestone.java
@@ -3,12 +3,15 @@ package io.github.rgdagir.mpr.models;
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 @ParseClassName("Milestone")
 public class Milestone extends ParseObject {
     private static final String KEY_CONVERSATION = "conversation";
     private static final String KEY_ACHIEVEMENT = "achievement";
+    private static final String KEY_USER = "user";
     private static final String KEY_POINTS_NEEDED = "pointsNeeded";
+    private static final String KEY_UNLOCKED = "unlocked";
 
     public Conversation getConversation() {
         return (Conversation) getParseObject(KEY_CONVERSATION);
@@ -26,12 +29,28 @@ public class Milestone extends ParseObject {
         put(KEY_ACHIEVEMENT, achievement);
     }
 
+    public ParseUser getUser() {
+        return getParseUser(KEY_USER);
+    }
+
+    public void setUser(ParseUser user) {
+        put(KEY_USER, user);
+    }
+
     public Integer getPointsNeeded() {
         return (Integer) getNumber(KEY_POINTS_NEEDED);
     }
 
     public void setPointsNeeded(Integer pointsNeeded) {
         put(KEY_POINTS_NEEDED, pointsNeeded);
+    }
+
+    public Boolean getUnlocked() {
+        return getBoolean(KEY_UNLOCKED);
+    }
+
+    public void setUnlocked(Boolean unlocked) {
+        put(KEY_UNLOCKED, unlocked);
     }
 
     public static class Query extends ParseQuery<Milestone> {
