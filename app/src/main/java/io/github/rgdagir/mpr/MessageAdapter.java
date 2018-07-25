b46e02bac67136209b7d497285e71fe5a6c4e636
Patricia Ouyang <pso91399@gmail.com>
2018-07-25 08:52:42 -0700

    Added default profile pics to chat messages

--- app/src/main/java/io/github/rgdagir/mpr/MessageAdapter.java
+++ app/src/main/java/io/github/rgdagir/mpr/MessageAdapter.java
@@ -1,18 +1,29 @@
 package io.github.rgdagir.mpr;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseImageView;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.List;
 
 import io.github.rgdagir.mpr.models.Message;
+
+import static com.parse.Parse.getApplicationContext;
 
 public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
@@ -54,8 +65,32 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
                 final ViewHolderOther viewHolderOther = (ViewHolderOther) holder;
                 viewHolderOther.mInfoOther.setText(message.getTimestamp());
                 viewHolderOther.mMessageOther.setText(message.getText());
+                displayDefaultProfilePicture(viewHolderOther.mProfilePic);
                 break;
         }
+    }
+
+    private void displayDefaultProfilePicture(final ParseImageView defaultImage) {
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereEqualTo("objectId", "Bhg8VXqMbu");
+        query.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser defaultUser, ParseException e) {
+                Glide.with(getApplicationContext()).load(defaultUser.getParseFile("profilePic").getUrl())
+                        .asBitmap().centerCrop().dontAnimate()
+                        .placeholder(R.drawable.ic_action_name)
+                        .error(R.drawable.ic_action_name)
+                        .into(new BitmapImageViewTarget(defaultImage) {
+                            @Override
+                            protected void setResource(Bitmap resource) {
+                                RoundedBitmapDrawable circularBitmapDrawable =
+                                        RoundedBitmapDrawableFactory.create(getApplicationContext().getResources(), resource);
+                                circularBitmapDrawable.setCircular(true);
+                                defaultImage.setImageDrawable(circularBitmapDrawable);
+                            }
+                        });
+            }
+        });
     }
 
     @Override
@@ -89,11 +124,13 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
     public class ViewHolderOther extends RecyclerView.ViewHolder {
         TextView mMessageOther;
         TextView mInfoOther;
+        ParseImageView mProfilePic;
 
         public ViewHolderOther(View itemView) {
             super(itemView);
             mMessageOther = itemView.findViewById(R.id.tvMessageOther);
             mInfoOther = itemView.findViewById(R.id.tvInfoOther);
+            mProfilePic = itemView.findViewById(R.id.ivProfilePic);
         }
     }
 }
